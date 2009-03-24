563a602a683d14433f8c137617a2ce520d1ba0eb
ivan.kristo <ivan.kristo@81452e38-189e-11de-b208-e5a9466b8568>
2009-03-24 18:29:10 +0000

    Provjera rada commita.

--- src/hr/fer/zemris/ui/lab1/VisaSila.java
+++ src/hr/fer/zemris/ui/lab1/VisaSila.java
@@ -5,6 +5,6 @@ public class VisaSila {
 	public static void main(String[] args) {
 		Svijet svijet = new Svijet(20, 10, 0.1, 0.1);
 		Agent agent = new Agent(svijet);
-		//agent.pomakniSe();
+		agent.pomakniSe();
 	}
 }
